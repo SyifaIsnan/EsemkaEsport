10a61b473e38650c28d526c95d8b5e67f469c985
Syifa <syifaisnantyana@gmail.com>
2025-04-18 13:29:54 +0700

    proses

--- EsemkaEsport/FormLogin.Designer.cs
+++ EsemkaEsport/FormLogin.Designer.cs
@@ -161,9 +161,9 @@
             // 
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Right;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(561, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(587, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(610, 450);
+            this.pictureBox1.Size = new System.Drawing.Size(584, 450);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 47;
             this.pictureBox1.TabStop = false;
